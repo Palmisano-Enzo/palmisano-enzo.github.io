7434296aa7797380ef70349b97b76bb6cc8867d0
Palmisano-enzo <enzo.palmisano@epfl.ch>
2026-01-15 21:27:32 +0100

    Vibecoding simple template

---
+++ site/js/lang.js
@@ -0,0 +1,23 @@
+let langData = {};
+let currentLang = "fr";
+
+fetch("data/lang.json")
+  .then(res => res.json())
+  .then(data => {
+    langData = data;
+    updateContent();
+  });
+
+function setLang(lang) {
+  currentLang = lang;
+  document.documentElement.lang = lang;
+  updateContent();
+}
+
+function updateContent() {
+  document.querySelectorAll("[data-key]").forEach(el => {
+    const key = el.getAttribute("data-key");
+    const value = key.split(".").reduce((obj, i) => obj[i], langData[currentLang]);
+    el.textContent = value;
+  });
+}
