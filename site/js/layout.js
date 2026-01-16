bc391b178e59223321850b6589f284d274f9fd3e
Palmisano-enzo <enzo.palmisano@epfl.ch>
2026-01-16 19:11:32 +0100

    Adding a active nav salection vizualisation

--- site/js/layout.js
+++ site/js/layout.js
@@ -6,3 +6,12 @@ function loadPartial(selector, url, callback) {
         if (callback) callback();
       });
   }
+
+  function highlightActivePage() {
+    const page = location.pathname.split("/").pop() || "index.html";
+  
+    document.querySelectorAll(".main-nav a").forEach(link => {
+      const href = link.getAttribute("href");
+      link.classList.toggle("active", href === page);
+    });
+  }
